013f40bbff40f38098863ecdab12a80c78d4ff0c
Zheng Wang <zheng.wang.rice@gmail.com>
2020-01-30 21:00:36 -0800

    test alpaca

--- debug.py
+++ debug.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 from common import *
+import alpaca_trade_api as tradeapi
+import time
+
+API_KEY = ''
+API_SECRET = ''
+APCA_API_BASE_URL = "https://api.alpaca.markets"
 
 
 def plot_buy_points(ticker):
@@ -14,8 +20,59 @@ def plot_buy_points(ticker):
     plt.show()
 
 
+def test_alpaca():
+    alpaca = tradeapi.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, 'v2')
+    # Time
+    clock = alpaca.get_clock()
+    closingTime = clock.next_close.replace().timestamp()
+    currTime = time.time()
+    print('Market is open:', clock.is_open)
+    print('Market close time:', closingTime)
+    print('Current time:', currTime)
+
+    # Account info
+    account = alpaca.get_account()
+    print('Account cash:', float(account.cash))
+    print('Account buying power:', float(account.buying_power))
+
+    # Submit buy order
+    alpaca.submit_order('TAL', 1, 'buy', 'market', 'day')
+    print('Buy order submitted')
+    orders = alpaca.list_orders(status='open')
+    while orders:
+        print('Wait for order to fill...')
+        time.sleep(1)
+        orders = alpaca.list_orders(status='open')
+
+    # Positions
+    positions = alpaca.list_positions()
+    print('Get %d positions' % (len(positions),))
+    for position in positions:
+        print(position)
+
+    # Account info
+    account = alpaca.get_account()
+    print('Account cash:', float(account.cash))
+    print('Account buying power:', float(account.buying_power))
+
+    # Submit sell order
+    alpaca.submit_order('TAL', 1, 'sell', 'market', 'day')
+    print('Sell order submitted')
+    orders = alpaca.list_orders(status='open')
+    while orders:
+        print('Wait for order to fill...')
+        time.sleep(1)
+        orders = alpaca.list_orders(status='open')
+
+    # Account info
+    account = alpaca.get_account()
+    print('Account cash:', float(account.cash))
+    print('Account buying power:', float(account.buying_power))
+
+
 def main():
-    plot_buy_points('AMEH')
+    #plot_buy_points('AMEH')
+    test_alpaca()
 
 
 if __name__ == '__main__':
