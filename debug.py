a9eef33bfcdb86025b6fa9ded183b7ce335786b0
Zheng Wang <zheng.wang.rice@gmail.com>
2020-02-21 21:42:13 -0800

    clean up debug

--- debug.py
+++ debug.py
@@ -9,18 +9,6 @@ import numpy as np
 import pandas as pd
 
 
-def plot_buy_points(ticker):
-    series = get_series(ticker)[1][-LOOK_BACK_DAY:]
-    pick_t, avg_return, threshold = get_picked_points(series)
-    print(get_header(ticker))
-    print('Average return: %.2f%%' % (avg_return * 100,))
-    print('Threshold: %.2f%%' % (threshold * 100,))
-    t = range(len(series))
-    plt.plot(t, series)
-    plt.plot(pick_t, series[pick_t], 'o')
-    plt.show()
-
-
 def test_alpaca():
     alpaca = tradeapi.REST(os.environ['ALPACA_PAPER_API_KEY'],
                            os.environ['ALPACA_PAPER_API_SECRET'],
@@ -37,18 +25,18 @@ def test_alpaca():
     # Account info
     account = alpaca.get_account()
     print(account)
-    exit(0)
+
     print('Account cash:', float(account.cash))
     print('Account equity:', float(account.equity))
 
     # Submit buy order
-    #alpaca.submit_order('TAL', 1, 'buy', 'market', 'day')
-    #print('Buy order submitted')
-    #orders = alpaca.list_orders(status='open')
-    #while orders:
-    #    print('Wait for order to fill...')
-    #    time.sleep(1)
-    #    orders = alpaca.list_orders(status='open')
+    alpaca.submit_order('TAL', 1, 'buy', 'market', 'day')
+    print('Buy order submitted')
+    orders = alpaca.list_orders(status='open')
+    while orders:
+        print('Wait for order to fill...')
+        time.sleep(1)
+        orders = alpaca.list_orders(status='open')
 
     # Positions
     positions = alpaca.list_positions()
@@ -76,7 +64,6 @@ def test_alpaca():
 
 
 def main():
-    #plot_buy_points('AMEH')
     test_alpaca()
 
 
