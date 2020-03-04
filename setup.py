2342f3fdcc1ac51f5bc70aa69db3d3c9c35c155b
Zheng Wang <zhengwang@google.com>
2020-03-03 16:52:54 -0800

    reorg it into a package

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     packages=['stock'],
     url='https://github.com/zhengwang1990/stock',
     license='MIT',
-    author='zhengwang',
+    author='Zheng Wang',
     author_email='zheng.wang.rice@gmail.com',
     description='A stock trading strategy',
-    install_requires=['tqdm', 'matplotlib', 'tabulate', 'yfinance', 'pandas', 'requests', 'numpy', 'retrying', 'sklearn', 'alpaca_trade_api',
-                      'ta', 'tensorflow']
+    install_requires=['tqdm', 'matplotlib', 'tabulate', 'yfinance', 'pandas',
+                      'requests', 'numpy', 'retrying', 'sklearn',
+                      'alpaca_trade_api', 'ta', 'tensorflow']
 )
