a9a80b645fb397941a270a6512d3346abd60f689
Zheng Wang <zheng.wang.rice@gmail.com>
2020-04-21 15:13:59 -0700

    Fix notification with partial filled positions

--- notification.py
+++ notification.py
@@ -1,5 +1,6 @@
 import alpaca_trade_api as tradeapi
 import argparse
+import collections
 import datetime
 import os
 import smtplib
@@ -8,6 +9,7 @@ import utils
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+Order = collections.namedtuple('Order', ['price', 'qty', 'value'])
 
 def _create_server(user, password):
     server = smtplib.SMTP('smtp.live.com', 587)
@@ -24,6 +26,20 @@ def _create_message(sender, receiver):
     return message
 
 
+def _get_trade_info(orders, side):
+    trade_info = {}
+    for order in orders:
+        symbol = order.symbol
+        filled_qty = int(order.filled_qty)
+        if order.side == side and filled_qty > 0:
+            old = trade_info.get(symbol, Order(0, 0, 0))
+            new_qty = filled_qty + old.qty
+            new_value = float(order.filled_avg_price) * filled_qty + old.value
+            new_price = new_value / new_qty
+            trade_info[symbol] = Order(new_price, new_qty, new_value)
+    return trade_info
+
+
 def send_summary(sender, receiver, bcc, user, password, alpaca):
     calendar = alpaca.get_calendar(start=datetime.date.today() - datetime.timedelta(days=15),
                                    end=datetime.date.today())
@@ -38,38 +54,30 @@ def send_summary(sender, receiver, bcc, user, password, alpaca):
         datetime.date.today(),)
     orders = alpaca.list_orders(status='closed', after=open_dates[0])
     prev_orders = alpaca.list_orders(status='closed', after=open_dates[1], until=open_dates[0])
-    buys, sells = [], {}
-    for order in orders:
-        if order.status == 'filled' and order.side == 'buy':
-            buys.append([order.symbol, float(order.filled_avg_price), int(order.filled_qty)])
-        if order.status == 'filled' and order.side == 'sell':
-            sells[order.symbol] = [float(order.filled_avg_price), int(order.filled_qty)]
-    for order in prev_orders:
-        if order.status == 'filled' and order.side == 'buy' and order.symbol in sells:
-            sells[order.symbol].append(float(order.filled_avg_price))
-
+    buys = _get_trade_info(orders, 'buy')
+    sells = _get_trade_info(orders, 'sell')
+    prev_buys = _get_trade_info(prev_orders, 'buy')
     sell_text, sell_html = '', ''
     total_gain = 0
-    for symbol, info in sells.items():
-        if len(info) != 3:
+    for symbol, sell_info in sells.items():
+        if symbol not in prev_buys:
             continue
-        gain = (info[0] - info[-1]) * info[1]
-        percent = info[0] / info[-1] - 1
+        buy_info = prev_buys[symbol]
+        gain = sell_info.value - buy_info.value
+        percent = sell_info.price / buy_info.price - 1
         sell_text += '%s: buy at %g, sell at %g, quantity %d, gain/loss %+.2f (%+.2f%%)\n' % (
-            symbol, info[-1], info[0], info[1], gain, percent * 100)
+            symbol, buy_info.price, sell_info.price, sell_info.qty, gain, percent * 100)
         sell_html += ('<tr> <th scope="row">%s</th> <td>%g</td> <td>%g</td> <td>%d</td> '
                       '<td style="color:%s">%+.2f (%+.2f%%)</td> </tr>') % (
-            symbol, info[-1], info[0], info[1],
+            symbol, buy_info.price, sell_info.price, sell_info.qty,
             'green' if gain >= 0 else 'red', gain, percent * 100)
         total_gain += gain
     buy_text, buy_html = '', ''
-    for info in buys:
-        if len(info) != 3:
-            continue
+    for symbol, buy_info in buys.items():
         buy_text += '%s: buy at %g, quantity %d, cost %.2f\n' % (
-            info[0], info[1], info[2], info[1] * info[2])
+            symbol, buy_info.price, buy_info.qty, buy_info.value)
         buy_html += '<tr> <th scope="row">%s</th> <td>%g</td> <td>%d</td> <td>%.2f</td> </tr>' % (
-            info[0], info[1], info[2], info[1] * info[2])
+            symbol, buy_info.price, buy_info.qty, buy_info.value)
     account = alpaca.get_account()
     equity = float(account.equity)
     account_text = 'Equity: %.2f\nCash: %s\nGain / Loss: %+.2f (%+.2f%%)\n' % (
